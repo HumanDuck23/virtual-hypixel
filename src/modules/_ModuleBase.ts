91ca464aa9a9c8dbdd22eca2445fd42a8388bdd6
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-02-11 14:44:25 +0100

    added info log to _ModuleBase.ts

--- src/modules/_ModuleBase.ts
+++ src/modules/_ModuleBase.ts
@@ -1,15 +1,19 @@
 import {Client} from "minecraft-protocol"
+import {Logger} from "../classes/Logger";
 
 export class _ModuleBase {
 
     name: string
     version: string
     client: Client
+    logger: Logger = new Logger()
 
     constructor(name: string, version: string, client: Client) {
         this.name = name
         this.version = version
         this.client = client
+
+        this.logger.info(`Loaded module: ${name} v${version}`)
     }
 
     onPacket(meta: any, data: any) {}

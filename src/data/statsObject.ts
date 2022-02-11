b7399861f5a4f8411b1838b0d5969e6a31d4ab62
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-02-11 20:55:57 +0100

    stats now show player name (including rank / plus color)

--- src/data/statsObject.ts
+++ src/data/statsObject.ts
@@ -2,7 +2,7 @@ import {utils} from "../utils"
 import {mcColors} from "./mcColors";
 
 export const statsObject = {
-    "DUELS_SUMO_DUEL": (player: any) => {
+    "DUELS_SUMO_DUEL" (player: any) {
         const d = player.stats.Duels
 
         const wins = d.sumo_duel_wins
@@ -14,6 +14,24 @@ export const statsObject = {
         const lText = utils.message.colorText(`L: ${losses}`, mcColors.RED)
         const wsText = utils.message.colorText(`WS: ${ws}`, mcColors.LIGHT_PURPLE)
         const bwsText = utils.message.colorText(`BWS: ${bws}`, mcColors.DARK_PURPLE)
-        return utils.message.colorText(`${wText} - ${lText} - ${wsText} - ${bwsText}`, mcColors.WHITE)
+        return utils.message.colorText(`${this.getPlayerText(player)} - ${wText} - ${lText} - ${wsText} - ${bwsText}`, mcColors.WHITE)
+    },
+
+
+    getPlayerText(player: any) {
+        if (player.monthlyPackageRank === "SUPERSTAR") {
+            // @ts-ignore
+            return utils.message.colorText(`[MVP${utils.message.colorTextCustomReset("++", mcColors[player.rankPlusColor], mcColors.GOLD)}] ${player.displayname}`, mcColors.GOLD)
+        } else if (player.newPackageRank) {
+            if (player.newPackageRank === "VIP" || player.newPackageRank === "VIP_PLUS") {
+                return utils.message.colorText(`[VIP${player.newPackageRank === "VIP_PLUS" ? utils.message.colorTextCustomReset("+", mcColors.GOLD, mcColors.GREEN) : ""}] ${player.displayname}`, mcColors.GREEN)
+            } else if (player.newPackageRank === "MVP" || player.newPackageRank === "MVP_PLUS") {
+                // @ts-ignore
+                return utils.message.colorText(`[MVP${player.newPackageRank === "MVP_PLUS" ? utils.message.colorTextCustomReset("+", mcColors[player.rankPlusColor], mcColors.AQUA) : ""}] ${player.displayname}`, mcColors.AQUA)
+            }
+        } else {
+            return utils.message.colorText(player.displayname, mcColors.GRAY)
+        }
+
     }
 }

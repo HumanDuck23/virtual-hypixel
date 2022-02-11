060df61f30bb685c0c17c99be4bee2982caaadf7
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-02-11 15:30:36 +0100

    added online status, current mode loading

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -5,6 +5,8 @@ import axios from "axios"
 export class Player {
 
     name?: string
+    currentMode?: string
+    online?: boolean
     uuid: string
 
     constructor(uuid: string) {
@@ -36,6 +38,27 @@ export class Player {
                 } else {
                     this.name = res.data[res.data.length - 1].name
                     resolve(true)
+                }
+            } catch (e) {
+                reject(e)
+            }
+        })
+    }
+
+    loadMode(apiKey: string) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(`https://api.hypixel.net/status?uuid=${this.uuid}&key=${apiKey}`)
+                if (res.data.success) {
+                    if (res.data.session.online) {
+                        this.online = true
+                        this.currentMode = res.data.session.mode
+                    } else {
+                        this.online = false
+                    }
+                    resolve(this.online)
+                } else {
+                    reject(`Error with hypixel api: ${res.status}`)
                 }
             } catch (e) {
                 reject(e)

ee564016aa1d249d2bce2db5154bec370c7d83e5
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-02-12 18:01:22 +0100

    created CommandModule.ts and make /rq command

---
+++ src/modules/CommandModule.ts
@@ -0,0 +1,29 @@
+import {Client} from "minecraft-protocol"
+import {configInterface} from "../interfaces/configInterface"
+import {Player} from "../classes/Player"
+import {_ModuleBase} from "./_ModuleBase"
+import {VirtualHypixel} from "../classes/VirtualHypixel"
+import {utils} from "../utils"
+import {mcColors} from "../data/mcColors"
+
+export class CommandModule extends _ModuleBase {
+
+    constructor(client: Client, virtual: VirtualHypixel) {
+        super("Commands",  "1.0.0", client, virtual)
+    }
+
+    onOutPacket(meta: any, data: any, toServer: Client) {
+        if (meta.name === "chat") {
+            if (data.message === "/rq") {
+                if (this.virtual.playerModule?.clientPlayer.currentMode)
+                    toServer.write("chat", { message: `/play ${this.virtual.playerModule?.clientPlayer.currentMode.toLowerCase()}` })
+                else
+                    utils.message.sendMessage(this.client, utils.message.colorText("You need to be in game to re-queue!", mcColors.RED, true))
+                return true
+            }
+        }
+
+        return false
+    }
+
+}

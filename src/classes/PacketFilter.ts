4037195aa1e06db7744c0d797f0ee6d5d4c27141
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-02-12 20:03:05 +0100

    filter obfuscated stuff

--- src/classes/PacketFilter.ts
+++ src/classes/PacketFilter.ts
@@ -8,14 +8,21 @@ export class PacketFilter {
         this.config = config
     }
 
-    handleIncomingPacket(meta: any, data: any) {
+    handleIncomingPacket(meta: any, data: any): [boolean, any] {
         // World Particles
-        if (meta.name === "world_particles" && !this.config.particles) return true
+        if (meta.name === "world_particles" && !this.config.particles) return [true, null]
 
         // Disabled mods
-        if (data.data && data.data.toString().toLowerCase().includes("hypixel") && this.config.enableMods) return true
-        if (data.channel && data.channel === "badlion:mods" && this.config.enableMods) return true
-        return false
+        if (data.data && data.data.toString().toLowerCase().includes("hypixel") && this.config.enableMods) return [true, null]
+        if (data.channel && data.channel === "badlion:mods" && this.config.enableMods) return [true, null]
+
+        if (meta.name === "scoreboard_team") {
+            if (data.team) data.team = data.team.replace("§k", "")
+            if (data.name) data.name = data.name.replace("§k", "")
+            if (data.prefix) data.prefix = data.prefix.replace("§k", "")
+        }
+
+        return [false, data]
     }
 
 }
